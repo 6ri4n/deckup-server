840903dc6c025635dde6827e94bbdbc4ac4ffbd2
brian <bri4n.huang@gmail.com>
2024-02-29 17:00:34 -0800

    add deck update endpoint

--- src/controllers/deck.js
+++ src/controllers/deck.js
@@ -42,7 +42,28 @@ const readDeck = asyncHandler(async (req, res) => {
   }
 });
 
-const updateDeck = asyncHandler(async (req, res) => {});
+const updateDeck = asyncHandler(async (req, res) => {
+  const userId = req.params.userId;
+  const { _id: deckId, deck: updatedDeck } = req.body;
+
+  try {
+    const foundUser = await User.findOneAndUpdate(
+      { _id: userId, "decks._id": deckId },
+      { $set: { "decks.$": updatedDeck } },
+      { new: true }
+    );
+
+    if (!foundUser) {
+      res.status(404);
+      throw new Error("User not found.");
+    }
+
+    res.status(200).json({ message: "Successfully updated the deck." });
+  } catch (err) {
+    res.status(500);
+    throw new Error(err.message);
+  }
+});
 
 const deleteDeck = asyncHandler(async (req, res) => {});
 
