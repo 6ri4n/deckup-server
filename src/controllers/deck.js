075b392e2fd2bde4e06a3da9311c02025f9a6b1b
brian <bri4n.huang@gmail.com>
2024-02-29 16:33:21 -0800

    add deck create endpoint

--- src/controllers/deck.js
+++ src/controllers/deck.js
@@ -1,6 +1,28 @@
+const User = require("../database/models/user");
 const asyncHandler = require("../utils/asyncHandler");
 
-const createDeck = asyncHandler(async (req, res) => {});
+const createDeck = asyncHandler(async (req, res) => {
+  const userId = req.params.userId;
+  const { deck: newDeck } = req.body;
+
+  try {
+    const foundUser = await User.findOneAndUpdate(
+      { _id: userId },
+      { $push: { decks: newDeck } },
+      { new: true }
+    );
+
+    if (!foundUser) {
+      res.status(404);
+      throw new Error("User not found.");
+    }
+
+    res.status(200).json({ message: "Successfully created a new deck." });
+  } catch (err) {
+    res.status(500);
+    throw new Error(err.message);
+  }
+});
 
 const readDeck = asyncHandler(async (req, res) => {});
 
