f91995db4c0838c7dea14f6f8aed7dca8190fd21
brian <bri4n.huang@gmail.com>
2024-02-29 17:07:27 -0800

    add deck delete endpoint

--- src/controllers/deck.js
+++ src/controllers/deck.js
@@ -65,6 +65,27 @@ const updateDeck = asyncHandler(async (req, res) => {
   }
 });
 
-const deleteDeck = asyncHandler(async (req, res) => {});
+const deleteDeck = asyncHandler(async (req, res) => {
+  const userId = req.params.userId;
+  const { _id: deckId } = req.body;
+
+  try {
+    const foundUser = await User.findOneAndUpdate(
+      { _id: userId },
+      { $pull: { decks: { _id: deckId } } },
+      { new: true }
+    );
+
+    if (!foundUser) {
+      res.status(404);
+      throw new Error("User not found.");
+    }
+
+    res.status(200).json({ message: "Successfully deleted the deck." });
+  } catch (err) {
+    res.status(500);
+    throw new Error(err.message);
+  }
+});
 
 module.exports = { createDeck, readDeck, updateDeck, deleteDeck };
