019f5c2e4dc9ea38db9386fc4647bd5a413b1c18
brian <bri4n.huang@gmail.com>
2024-03-02 21:14:12 -0800

    fix client port number

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const deckRoute = require("./src/routes/deck");
 connectDB(process.env.DB_URL);
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
