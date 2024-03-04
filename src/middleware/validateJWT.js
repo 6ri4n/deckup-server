182dacb5bba980dc55e954b2949a5724f4d98c05
brian <bri4n.huang@gmail.com>
2024-03-03 23:03:29 -0800

    add additional error checking for validateJWT

--- src/middleware/validateJWT.js
+++ src/middleware/validateJWT.js
@@ -5,23 +5,31 @@ const validateJWT = (req, res, next) => {
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     res.status(403);
-    throw new Error("Unauthorized: token is invalid or empty");
+    throw new Error("Unauthorized: Token is invalid or empty.");
   }
 
-  const token = authHeader.substring(7);
+  const accessToken = authHeader.substring(7);
   let payload;
 
   try {
-    payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-  } catch (error) {
-    res.status(403);
-    throw new Error("Unauthorized: token does not match");
-  }
+    payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
 
-  const now = Date.now() / 1000; // Convert to seconds
-  if (!payload.exp || payload.exp <= now) {
-    res.status(403);
-    throw new Error("Unauthorized: token has expired");
+    const now = Date.now() / 1000; // Convert to seconds
+    if (!payload.exp || payload.exp <= now) {
+      res.status(403);
+      throw new Error("Unauthorized: Token has expired.");
+    }
+  } catch (error) {
+    if (error.name === "JsonWebTokenError") {
+      res.status(403);
+      throw new Error("Unauthorized: Token is invalid.");
+    } else if (error.name === "TokenExpiredError") {
+      res.status(403);
+      throw new Error("Unauthorized: Token has expired.");
+    } else {
+      res.status(500);
+      throw new Error("Internal Server Error.");
+    }
   }
 
   req.body.user = payload;
